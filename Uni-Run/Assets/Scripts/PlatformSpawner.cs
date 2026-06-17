741d8419c091cce5499fdddbfe1d92d96f89ff76
Sunjin.Kim <ks42ks@naver.com>
2026-06-17 10:50:40 +0900

    Run 03

--- Uni-Run/Assets/Scripts/PlatformSpawner.cs
+++ Uni-Run/Assets/Scripts/PlatformSpawner.cs
@@ -22,9 +22,40 @@ public class PlatformSpawner : MonoBehaviour {
 
     void Start() {
         // 변수들을 초기화하고 사용할 발판들을 미리 생성
+        platforms = new GameObject[count]; //참조한 것으로 주소를 가르킴
+        for (int i = 0; i<count; i++)
+        {
+            platforms[i] = Instantiate(platformPrefab, poolPosition, //실제 메모리는 Instantiate 에서만듬
+                                         Quaternion.identity);
+
+        }
+        lastSpawnTime = 0f;
+        timeBetSpawn = 0f;
     }
 
     void Update() {
         // 순서를 돌아가며 주기적으로 발판을 배치
+        if(GameManager.instance.isGameover)
+        {
+            return; //디펜시브프로그래밍
+        }
+        if(Time.time >= lastSpawnTime + timeBetSpawn) //현재시간이 마지막만든시간+ 간격이 조건을 갖추면 생성
+        {
+            lastSpawnTime = Time.time;
+            timeBetSpawn = Random.Range(timeBetSpawnMin, timeBetSpawnMax); //정수는 max미포함, 실수는 포함
+
+            float yPos = Random.Range(yMin, yMax);
+
+            platforms[currentIndex].SetActive(false);
+            platforms[currentIndex].SetActive(true); //껐다키는 이유는 OnEnable 작동하기 위해서
+
+            platforms[currentIndex].transform.position = new Vector2(xPos, yPos);
+
+            currentIndex++;
+            if(currentIndex>=count)
+            {
+                currentIndex = 0;
+            }
+        }
     }
 }
